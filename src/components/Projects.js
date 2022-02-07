45a4fd52c261de6e0c982d222ae0cf99d28b42e2
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-07 13:03:16 -0500

    add 2 projects, edit layouts

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,7 +15,7 @@ export default function Projects() {
 
             <div className='flex flex-wrap justify-center pt-2'>
 
-                <div className="m-2 w-1/3">
+                <div className="w-80 m-2 ">
                     <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 hover:bg-gray-300 hover:ring-gray-300 hover:transition-colors duration-300 ease-in-out"
                         onClick={handleClick}
                     >
@@ -27,7 +27,53 @@ export default function Projects() {
                             <p className='text-base my-2 sm:text-xl text-gray-900 '>
                                 MaintenancePro
                             </p>
-                            <p className='text-xs text-left sm:text-base text-gray-600 w-auto inline-block border-b-2'>
+                            <p className='text-xs text-left sm:text-xs text-gray-600 w-auto inline-block border-b-2 h-48'>
+                                A fully deployed MERN stack app created from scratch to serve to a real client for testing purpose. MaintenancePro is aComputerized Maintenance Management System that assist property managers to handle their operations in effi cient manner.
+                                <br></br>
+                                <br></br>
+                                Backend: MongoDb, NodeJs, Express, Heroku, JavaScript, TypeScript
+                                Frontend: ReactJS, Auth0, Mailgun, Recharts, Bulma , CircleCI, Netlify
+                                Testing: Jest, Storybook, Cypress
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-80 m-2">
+                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 hover:bg-gray-300 hover:ring-gray-300 hover:transition-colors duration-300 ease-in-out"
+                        onClick={handleClick}
+                    >
+                        <div className="">
+                            <img src={myPicture} alt="profile"
+                                className="w-64 mx-auto shadow-xl rounded-xl drop-shadow-sm" />
+                        </div>
+                        <div className='text-center mt-5'>
+                            <p className='text-base my-2 sm:text-xl text-gray-900 '>
+                                Bubbles
+                            </p>
+                            <p className='text-xs text-left sm:text-xs text-gray-600 w-auto inline-block border-b-2 h-48'>
+                                Bubbles is a fancy looking bubble tea ordering app for a fi ctitious joint. User can place an order and receive SMS confi rmation ofthe order. App owner also get a SMS notifi cation regarding order details. App uses database schema created by us and is a full stackapplication.
+                                <br></br>
+                                <br></br>
+                                NodeJS, Express, jQuery, CSS, Bulma (CSS framework), Twilio (SMS API), PostgreSQL, Version Control and working in multiplebranches
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-80 m-2">
+                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 hover:bg-gray-300 hover:ring-gray-300 hover:transition-colors duration-300 ease-in-out"
+                        onClick={handleClick}
+                    >
+                        <div className="">
+                            <img src={myPicture} alt="profile"
+                                className="w-64 mx-auto shadow-xl rounded-xl drop-shadow-sm" />
+                        </div>
+                        <div className='text-center mt-5'>
+                            <p className='text-base my-2 sm:text-xl text-gray-900 '>
+                                MaintenancePro
+                            </p>
+                            <p className='text-xs text-left sm:text-xs text-gray-600 w-auto inline-block border-b-2 h-48'>
                                 A fully deployed MERN stack app created from scratch to serve to a real client for testing purpose. MaintenancePro is aComputerized Maintenance Management System that assist property managers to handle their operations in effi cient manner.
                                 Backend: MongoDb, NodeJs, Express, Heroku, JavaScript, TypeScript
                                 Frontend: ReactJS, Auth0, Mailgun, Recharts, Bulma , CircleCI, Netlify
@@ -37,92 +83,71 @@ export default function Projects() {
                     </div>
                 </div>
 
-                <div className="m-2">
-                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 ">
+                <div className="w-80 m-2">
+                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 hover:bg-gray-300 hover:ring-gray-300 hover:transition-colors duration-300 ease-in-out"
+                        onClick={handleClick}
+                    >
                         <div className="">
                             <img src={myPicture} alt="profile"
                                 className="w-64 mx-auto shadow-xl rounded-xl drop-shadow-sm" />
                         </div>
                         <div className='text-center mt-5'>
-                            <p className='text-xl sm:text-2xl text-gray-900 '>
-                                Ujay Sidhpura
+                            <p className='text-base my-2 sm:text-xl text-gray-900 '>
+                                MaintenancePro
                             </p>
-                            <p className='text-xs sm:text-base text-gray-600 p-4 w-auto inline-block border-b-2'>
-                                Full-Stack Developer
+                            <p className='text-xs text-left sm:text-xs text-gray-600 w-auto inline-block border-b-2 h-48'>
+                                A fully deployed MERN stack app created from scratch to serve to a real client for testing purpose. MaintenancePro is aComputerized Maintenance Management System that assist property managers to handle their operations in effi cient manner.
+                                Backend: MongoDb, NodeJs, Express, Heroku, JavaScript, TypeScript
+                                Frontend: ReactJS, Auth0, Mailgun, Recharts, Bulma , CircleCI, Netlify
+                                Testing: Jest, Storybook, Cypress
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div className="m-2">
-                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 ">
+                <div className="w-80 m-2">
+                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 hover:bg-gray-300 hover:ring-gray-300 hover:transition-colors duration-300 ease-in-out"
+                        onClick={handleClick}
+                    >
                         <div className="">
                             <img src={myPicture} alt="profile"
                                 className="w-64 mx-auto shadow-xl rounded-xl drop-shadow-sm" />
                         </div>
                         <div className='text-center mt-5'>
-                            <p className='text-xl sm:text-2xl text-gray-900 '>
-                                Ujay Sidhpura
+                            <p className='text-base my-2 sm:text-xl text-gray-900 '>
+                                MaintenancePro
                             </p>
-                            <p className='text-xs sm:text-base text-gray-600 p-4 w-auto inline-block border-b-2'>
-                                Full-Stack Developer
+                            <p className='text-xs text-left sm:text-xs text-gray-600 w-auto inline-block border-b-2 h-48'>
+                                A fully deployed MERN stack app created from scratch to serve to a real client for testing purpose. MaintenancePro is aComputerized Maintenance Management System that assist property managers to handle their operations in effi cient manner.
+                                Backend: MongoDb, NodeJs, Express, Heroku, JavaScript, TypeScript
+                                Frontend: ReactJS, Auth0, Mailgun, Recharts, Bulma , CircleCI, Netlify
+                                Testing: Jest, Storybook, Cypress
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div className="m-2">
-                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 ">
+                <div className="w-80 m-2">
+                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 hover:bg-gray-300 hover:ring-gray-300 hover:transition-colors duration-300 ease-in-out"
+                        onClick={handleClick}
+                    >
                         <div className="">
                             <img src={myPicture} alt="profile"
                                 className="w-64 mx-auto shadow-xl rounded-xl drop-shadow-sm" />
                         </div>
                         <div className='text-center mt-5'>
-                            <p className='text-xl sm:text-2xl text-gray-900 '>
-                                Ujay Sidhpura
+                            <p className='text-base my-2 sm:text-xl text-gray-900 '>
+                                MaintenancePro
                             </p>
-                            <p className='text-xs sm:text-base text-gray-600 p-4 w-auto inline-block border-b-2'>
-                                Full-Stack Developer
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="m-2">
-                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 ">
-                        <div className="">
-                            <img src={myPicture} alt="profile"
-                                className="w-64 mx-auto shadow-xl rounded-xl drop-shadow-sm" />
-                        </div>
-                        <div className='text-center mt-5'>
-                            <p className='text-xl sm:text-2xl text-gray-900 '>
-                                Ujay Sidhpura
-                            </p>
-                            <p className='text-xs sm:text-base text-gray-600 p-4 w-auto inline-block border-b-2'>
-                                Full-Stack Developer
+                            <p className='text-xs text-left sm:text-xs text-gray-600 w-auto inline-block border-b-2 h-48'>
+                                A fully deployed MERN stack app created from scratch to serve to a real client for testing purpose. MaintenancePro is aComputerized Maintenance Management System that assist property managers to handle their operations in effi cient manner.
+                                Backend: MongoDb, NodeJs, Express, Heroku, JavaScript, TypeScript
+                                Frontend: ReactJS, Auth0, Mailgun, Recharts, Bulma , CircleCI, Netlify
+                                Testing: Jest, Storybook, Cypress
                             </p>
                         </div>
                     </div>
                 </div>
-
-                <div className="m-2">
-                    <div className="flex flex-col justify-center max-w-ms mx-auto bg-white shadow-xl rounded-xl p-5 ">
-                        <div className="">
-                            <img src={myPicture} alt="profile"
-                                className="w-64 mx-auto shadow-xl rounded-xl drop-shadow-sm" />
-                        </div>
-                        <div className='text-center mt-5'>
-                            <p className='text-xl sm:text-2xl text-gray-900 '>
-                                Ujay Sidhpura
-                            </p>
-                            <p className='text-xs sm:text-base text-gray-600 p-4 w-auto inline-block border-b-2'>
-                                Full-Stack Developer
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-
 
             </div>
         </div>
