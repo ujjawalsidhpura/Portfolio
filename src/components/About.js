7b5c34c66e3f58f4e53dfc2400902c321aa714ad
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-07 11:54:47 -0500

    about me section edits

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,8 @@ export default function About() {
         <div className="flex flex-col justify-center max-w-ms mx-auto my-10 bg-white shadow-xl rounded-xl p-5 ">
 
             <div className="max-mx-2xl mt-5 mx-auto ">
-                <p className="text-xl sm:text-2xl text-gray-900 font-bold text-center">
-                    About me
-                </p>
-                <p className="text-xl sm:text-base text-gray-700 text-center ">
+
+                <p className="text-xl sm:text-base text-gray-700 ">
                     I am a Civil Engineer turned into a Developer to reinvent my career. I have worked over 5 years as Facility Manager of a Marriottbrand hotel and handled few major projects, including $1 million+ renovation and integrating an Asset Management Software intoSOP of engineering department. From my experience as FM and understanding needs of a FM, I created MaintenancePro app whichaims at small budget properties who wants to shift to such softwares but wants it to be less complex, intuitive and aff ordable.
                     <br>
                     </br>
@@ -18,11 +16,11 @@ export default function About() {
                     My hobbies includes long distance running, biking, sauna, watching Sitcoms, camping and exploring various coffee joints. Yes!, I am coffeeholic!
                 </p>
 
-                <ScrollIntoView selector=''>
+                {/* <ScrollIntoView selector=''>
                     <div className='mx-auto py-5 '>
                         <FaChevronDown className='animate-bounce mx-auto text-3xl text-gray-800' />
                     </div>
-                </ScrollIntoView>
+                </ScrollIntoView> */}
 
             </div>
         </div>
